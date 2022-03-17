08ca13bd18557aeba07bdc987889502460b477d1
Robson Leal <devleal@hotmail.com>
2022-03-17 20:38:39 -0300

    Criando primeiro modelo para os posts do blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published')
+    ordering = ('-published',)
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS = (
+        ('rascunho', 'Rascunho'),
+        ('publicado', 'Publicado')
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    published = models.DateField(default=timezone.now)
+    create_at = models.DateField(auto_now_add=True)
+    changed_at = models.DateField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS, default='rascunho')
+
+    def __str__(self):
+        return self.title
