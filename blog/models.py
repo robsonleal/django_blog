e8809baa83cefa3692317124a01145160c845bda
Robson Leal <devleal@hotmail.com>
2022-03-20 09:57:11 -0300

    Utilizando absolute URL e deixando URL amigavel com slug

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -27,6 +28,8 @@ class Post(models.Model):
 
     # objects = models.Manager()
     # published = PublisherManager()
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.slug])
 
     def __str__(self):
         return f"{self.title}"
