b0b27d504e7af2f48f158ee35af39b925a04eb08
Robson Leal <devleal@hotmail.com>
2022-03-20 13:23:27 -0300

    Criando template para inserir um novo post, usando CBV para configurar a URL

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import BlogListView, BlogDetailView
+from blog.views import BlogListView, BlogDetailView, BlogCreateView
 
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-    path('post/<slug:slug>', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<slug:slug>/', BlogDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 
@@ -12,3 +13,9 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = '__all__'
