d3db7028d2ef3ec5da47b89607eef59c45555ca9
Robson Leal <devleal@hotmail.com>
2022-03-20 09:47:56 -0300

    Fazendo pagina de detalhe do post utilizando Class Based View

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,11 +112,13 @@ MEDIA_ROOT = 'media'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    'static'
-]
 STATIC_URL = 'static/'
+# Caminho para arquivos estáticos que não estão vinculado a nenhum app
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+# Utilizado para reunir os arquivos estáticos em um só local
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
