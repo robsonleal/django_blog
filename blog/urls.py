f8e4d8ed7c74f0301a8ff625d329178c1bd13d53
Robson Leal <devleal@hotmail.com>
2022-03-21 08:07:30 -0300

    Implementando Update View com CBV

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from blog.views import BlogListView, BlogDetailView, BlogCreateView
+from blog.views import \
+    BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<slug:slug>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/edit/<slug:slug>/', BlogUpdateView.as_view(), name='post_edit'),
 ]
