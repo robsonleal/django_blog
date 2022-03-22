5648eca22eb4e1ddbae668b4c3faac97e46dd6b7
Robson Leal <devleal@hotmail.com>
2022-03-22 20:20:37 -0300

    Incluindo e-mail ao cadastro de usuarios e fazendo uma validacao para nao permitir um email repetido

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserCreationFormWithEmail(UserCreationForm):
+    email = forms.EmailField(required=True, help_text='Obrigatório.\
+        Digite seu e-mail válido.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('E-mail já cadastrado!')
+        return email
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from accounts.forms import UserCreationFormWithEmail
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     success_url = reverse_lazy('home')
     template_name = 'accounts/signup.html'
