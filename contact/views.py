5ceced11ea73a43e4e5b95a15cd424805564da99
Robson Leal <devleal@hotmail.com>
2022-04-03 12:35:14 -0300

    Criando sistema para envio de email automatico ao preencher formulario de contato

--- contact/views.py
+++ contact/views.py
@@ -5,26 +5,39 @@ from django.core.mail import EmailMessage
 
 def contact(request):
     send = False
+    alert = {}
 
     form = ContactForm(request.POST or None)
     if form.is_valid():
-        nome = request.POST.get('nome', '')
+        name = request.POST.get('name', '')
         email = request.POST.get('email', '')
-        mensagem = request.POST.get('mensagem', '')
+        message = request.POST.get('menssage', '')
         email = EmailMessage(
             'Mensagem do blog Django',
-            'De: {} <{}>\nEscreveu:\n\n{}'.format(nome, email, mensagem),
+            'De: {} <{}>\nEscreveu:\n\n{}'.format(name, email, message),
+            'no-reply@inbox.mailtrap.io',
+            ['rrobson9@gmail.com'],
+            reply_to=[email]
         )
 
         try:
             email.send()
+            alert = {
+                'tag': 'success',
+                'message': 'Mensagem enviada com sucesso!'
+                }
             send = True
 
         except:
             send = False
+            alert = {
+                'tag': 'danger',
+                'message': 'Erro ao enviar mensagem!'
+            }
 
     context = {
         'form': form,
+        'alert': alert,
         'success': send
     }
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# E-mail configuration
+EMAIL_HOST = 'smtp.mailtrap.io'
+EMAIL_HOST_USER = '9a899b4c97cf17'
+EMAIL_HOST_PASSWORD = '51c109d1aae9a2'
+EMAIL_PORT = '2525'
